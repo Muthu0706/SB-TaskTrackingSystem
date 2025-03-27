dd4863607ef61c74a5748829df0fc8664fbcaa64
MUTHURAJ.P <111838220+Muthu0706@users.noreply.github.com>
2025-03-27 13:01:00 +0530

    Update WebConfig.java

--- src/main/java/com/tasktrackingsystem/config/WebConfig.java
+++ src/main/java/com/tasktrackingsystem/config/WebConfig.java
@@ -4,20 +4,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 import com.tasktrackingsystem.constants.TaskConstants;
- 
+
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-	
-@Override
-public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping(TaskConstants.CROSSMAPPING) // /**
-        .allowedOrigins(TaskConstants.TASK_TRACKING_SYSTEM_UI_API, 
-                        TaskConstants.TASK_TRACKING_SYSTEM_PRODUCTION_API) // Allow both dev and prod
-        .allowedMethods(TaskConstants.API_MAPPING) // Allowed methods
-        .allowCredentials(true);
-}
-}
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping(TaskConstants.CROSSMAPPING) // Allow any path
+            .allowedOrigins(TaskConstants.TASK_TRACKING_SYSTEM_UI_API, 
+                           TaskConstants.TASK_TRACKING_SYSTEM_PRODUCTION_API) // Allow both dev and prod
+            .allowedMethods(TaskConstants.API_MAPPING) // Allowed methods
+            .allowedHeaders("*") // Allow all headers
+            .allowCredentials(true); // Allow credentials (if needed)
+    }
+}
